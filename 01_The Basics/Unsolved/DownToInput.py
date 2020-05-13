7b900b7e983a3e8d8ac86194f86be1b8504eb1b6
Michael Harvanek <mharvanek@gmail.com>
2020-05-12 19:55:36 -0500

    organized folders solved vs unsolved

---
+++ 01_The Basics/Unsolved/DownToInput.py
@@ -0,0 +1,10 @@
+# Take input of you and your neighbor's name
+
+
+# Take input how long each of you have been gaming
+
+
+# Add total years
+
+
+# Print results as full sentence with your name, your neighbor's name, and the total of how long you both have been gaming
